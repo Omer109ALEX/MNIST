dbcab57bac1678d501d9dbe1f2f2237ca4d24d31
Omer109ALEX <103755009+Omer109ALEX@users.noreply.github.com>
2022-12-10 18:59:01 +0200

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,10 +1,4 @@
-"""
-Title: Simple MNIST convnet
-Author: [fchollet](https://twitter.com/fchollet)
-Date created: 2015/06/19
-Last modified: 2020/04/21
-Description: A simple convnet that achieves ~99% test accuracy on MNIST.
-"""
+
 
 """
 ## Setup
